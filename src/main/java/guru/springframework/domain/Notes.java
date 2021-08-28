92791c4061d0b424903ee12e89fb5b230731a601
jucron <jucron@gmail.com>
2021-08-28 18:09:05 +0100

    Creating IndexController and Notes entities and applying OneToOne relationships.

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,41 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne //Not specifying a type, makes the Recipe as OWNER.
+    private Recipe recipe;
+
+    @Lob //Large-objects : makes this field to be a large String object (CLOB - character-large-object)
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
