946513531b4b5e22805701a339e4a62804db3591
jucron <jucron@gmail.com>
2021-10-20 10:27:14 +0100

    Implemented MVC Exception Handler for 400 NumberFormatException.

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -60,6 +60,15 @@ public class RecipeControllerTest {
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
     }
+    @Test
+    public void testGetRecipeNotNumberFormat() throws Exception {
+
+        when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+
+        mockMvc.perform(get("/recipe/aa/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 
     @Test
     public void testGetNewRecipeForm() throws Exception {
